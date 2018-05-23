a4084ae632eebb6a45b68691b2782feb0d8f2f07
Kelsey Swagler <kelsey.swagler@gmail.com>
2018-05-23 16:01:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object ({artist: 'song title'});
-function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song;
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
   return playlist;
 }
